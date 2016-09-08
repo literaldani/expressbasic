6e0411da873c49a031f297cdbb9f39a85c06d9b1
Daniel Arocha <daniarochah@gmail.com>
2016-09-08 05:58:07 +0000

    tengo sueno mejor parar ya

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res) {
-    res.send("conectado");
+app.get("/", function (req, res) {
+    res.send("Hola, este es el inicio del ejercicio");
+});
+
+app.get("/speak/:animal", function (req, res) {
+    
+    res.send(req.params.animal);
 });
 
 app.listen(process.env.PORT,process.env.IP);
