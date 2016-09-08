21f8be88d6434b33d26d475649b1fc4dcaa9cd89
Daniel Arocha <daniarochah@gmail.com>
2016-09-08 21:03:51 +0000

    ejercicio finalizado

--- app.js
+++ app.js
@@ -7,8 +7,34 @@ app.get("/", function (req, res) {
 });
 
 app.get("/speak/:animal", function (req, res) {
+    var sounds ={
+        dog: "Woof",
+        pig: "Oink",
+        cow: "Moo",
+        cat: "I hate you"
+        };
     
-    res.send(req.params.animal);
+    var animal = req.params.animal.toLowerCase();
+    var sound = sounds[animal];
+    
+    res.send("The " + animal +  " says " + sound);
+    
+});
+
+app.get("/repeat/:word/:times", function(req, res) {
+    var times = Number(req.params.times);
+    var word = req.params.word;
+    var result = "";
+    for (var i = 0; i < times; i++ ) {
+        result += word + " ";
+    }
+    
+    res.send(result);
+    
+});
+
+app.get("*", function(req, res) {
+    res.send("no existe amigo");
 });
 
 app.listen(process.env.PORT,process.env.IP);
